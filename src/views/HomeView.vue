35ff5a58b525182a83c17b3fa59d0b2cf48c6ff8
piao2080 <76135481@qq.com>
2025-02-11 23:54:29 +0800

    修改文件5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home">
+    <h1>Jenkins自动化部署</h1>
     <h1>Hello world 持续集成持续部署111A11</h1>
     <h1>Hello world 持续集成持续部署222B22</h1>
     <h1>Hello world 持续集成持续部署333C33</h1>
+
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
