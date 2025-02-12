931c4ae8e3870c00ed4b5a03c284bed8d9bb01e7
piao2080 <76135481@qq.com>
2025-02-12 15:12:11 +0800

    项目流水线测试开始7

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@
     <h1>Hello world 持续集成持续部署111A11</h1>
     <h1>Hello world 持续集成持续部署222B22</h1>
     <h1>Hello world 持续集成持续部署333C33</h1>
+     <h1>上次自动化部署失败了Why!</h1>
 
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
